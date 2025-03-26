6c7b9fdd00c54093fc1b0e3d0b41899e131fad09
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2025-03-26 11:21:44 +0100

    Token Service and AuthHttpClientHandler

---
+++ HobbyMatchApp/Auth/TokenService/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace HobbyMatch.App.Auth.TokenService
+{
+	/// <summary>
+	/// This interface is responsible for managing the access token
+	/// </summary>
+	public interface ITokenService
+	{
+		Task SetAccessTokenAsync(string token);
+		Task<string?> GetAccessTokenAsync();
+		Task ClearAccessTokenAsync();
+		Task<IEnumerable<Claim>> GetClaimsFromTokenAsync();
+	}
+}
