876b29d6461f4f70b41035e33d202805615b961a
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2025-03-29 23:20:02 +0100

    User Service

---
+++ HobbyMatchApp/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using HobbyMatch.Model.Entities;
+
+namespace HobbyMatch.App.Services
+{
+	public interface IUserService
+	{
+		public Task<User[]?> GetUsersAsync();
+		public Task<User?> GetUserAsync(int id);
+		public Task<User?> EditUserAsync(int id, User user); // TODO: Decide what to send as content. User object or just the fields that need to be updated. In that case user would be determined by JWT token.
+	}
+}
