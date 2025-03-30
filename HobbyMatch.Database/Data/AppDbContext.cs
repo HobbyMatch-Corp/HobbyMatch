370bbc576ffdfc592e2862a53ddb80e55febe8e9
DrKuba2003 <kubadrazan2@gmail.com>
2025-03-31 01:26:48 +0200

    Added database seeding

--- HobbyMatch.Database/Data/AppDbContext.cs
+++ HobbyMatch.Database/Data/AppDbContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Security.Claims;
 
 namespace HobbyMatch.Database.Data
 {
@@ -16,5 +18,90 @@ namespace HobbyMatch.Database.Data
         public DbSet<User> AppUsers { get; set; }
         public DbSet<BusinessClient> BusinessClients { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // Database seeding
+            optionsBuilder.UseSeeding((context, _) =>
+            {
+                var user = context.Set<User>().FirstOrDefault(u => u.Email == "user@test.com");
+                if (user == null)
+                {
+
+                    var userManager = context.GetService<UserManager<Organizer>>();
+                    var newUser = new User
+                    {
+                        UserName = "TestUser",
+                        Email = "user@test.com"
+                    };
+
+                    userManager.CreateAsync(newUser, "Pass123!").Wait();
+                    userManager.AddClaimsAsync(newUser, new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "Test User")
+                    }).Wait();
+                }
+
+                var bclient = context.Set<User>().FirstOrDefault(u => u.Email == "businessclient@test.com");
+                if (bclient == null)
+                {
+
+                    var userManager = context.GetService<UserManager<Organizer>>();
+
+                    var newBC = new BusinessClient
+                    {
+                        UserName = "TestBusinessClient",
+                        Email = "businessclient@test.com",
+                        TaxID = "taxid_2147743"
+                    };
+
+                    userManager.CreateAsync(newBC, "Pass123!").Wait();
+                    userManager.AddClaimsAsync(newBC, new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "Business Client")
+                    }).Wait();
+                }
+            })
+            .UseAsyncSeeding(async (context, _, ct) =>
+            {
+                var user = await context.Set<User>().FirstOrDefaultAsync(u => u.Email == "user@test.com", ct);
+                if (user == null)
+                {
+
+                    var userManager = context.GetService<UserManager<Organizer>>();
+                    var newUser = new User
+                    {
+                        UserName = "TestUser",
+                        Email = "user@test.com"
+                    };
+
+                    await userManager.CreateAsync(newUser, "Pass123!");
+                    await userManager.AddClaimsAsync(newUser, new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "Test User")
+                    });
+                }
+
+                var bclient = await context.Set<User>().FirstOrDefaultAsync(u => u.Email == "businessclient@test.com");
+                if (bclient == null)
+                {
+
+                    var userManager = context.GetService<UserManager<Organizer>>();
+                    var newBC = new BusinessClient
+                    {
+                        UserName = "TestBusinessClient",
+                        Email = "businessclient@test.com",
+                        TaxID = "taxid_2147743"
+                    };
+
+                    await userManager.CreateAsync(newBC, "Pass123!");
+                    await userManager.AddClaimsAsync(newBC, new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "Business Client")
+                    });
+                }
+            });
+        }
     }
 }
