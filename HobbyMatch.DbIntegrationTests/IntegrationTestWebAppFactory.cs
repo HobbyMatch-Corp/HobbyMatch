46917f9534ca09fcf358817eb5026d1fb84741c4
DrKuba2003 <kubadrazan2@gmail.com>
2025-04-03 23:24:46 +0200

    Create db integration test project Move Test projects to new folder Make HobbyMatch.API Program.cs public

---
+++ HobbyMatch.DbIntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using HobbyMatchAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyMatch.DbIntegrationTests
+{
+    public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
+    {
+    }
+}
