5570fc31be4f1d9de0f42f3959ba09601e4d0592
Avesay <juliak@jkkomputer.com.pl>
2025-04-06 15:22:48 +0200

    Add Events controller

---
+++ HobbyMatchAPI/Controllers/EventsController.cs
@@ -0,0 +1,32 @@
+﻿using HobbyMatch.BL.DTOs.Auth;
+using HobbyMatch.BL.Services.Auth.Account;
+using HobbyMatch.BL.Services.BusinessClient;
+using HobbyMatch.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HobbyMatch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        /*private readonly ;
+        public EventsController()
+        {
+            
+        }
+
+        [HttpPost("eventsignin")]
+        public async Task<T> EventSignin()
+        {
+            
+        }
+        [HttpPost("eventsignout")]
+        public async Task<T> EventSignout()
+        {
+            
+        }*/
+    }
+}
