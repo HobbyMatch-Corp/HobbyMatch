a1f3cc750c905923cb08bc02573aa63e8ccc23ed
Avesay <juliak@jkkomputer.com.pl>
2025-04-06 14:39:57 +0200

    Add Event sign in/out interface and create class

---
+++ HobbyMatchApp/Services/EventApiService.cs
@@ -0,0 +1,17 @@
+﻿using HobbyMatch.BL.Models.Auth;
+
+namespace HobbyMatch.App.Services
+{
+    public class EventApiService : IEventApiService
+    {
+        public Task<string?> EventSigninAsync(string EventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> EventSignoutAsync(string EventId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HobbyMatchApp/Services/IEventApiService.cs
@@ -0,0 +1,10 @@
+﻿using HobbyMatch.BL.Models.Auth;
+
+namespace HobbyMatch.App.Services
+{
+    public interface IEventApiService
+    {
+        Task<string?> EventSigninAsync(string EventId);
+        Task<string?> EventSignoutAsync(string EventId);
+    }
+}
