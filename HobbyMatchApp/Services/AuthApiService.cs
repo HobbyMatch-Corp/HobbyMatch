d1007ad4de8c9fad06395fac8a44c6ef4fd3ad9d
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2025-04-07 12:50:11 +0200

    review fix

--- HobbyMatchApp/Services/AuthApiService.cs
+++ HobbyMatchApp/Services/AuthApiService.cs
@@ -16,7 +16,8 @@ namespace HobbyMatch.App.Services
 		public async Task<AuthResult?> LoginAsync(string email, string password)
 		{
 			AuthResult? authResult = null;
-			var response = await _httpClient.PostAsJsonAsync("/api/auth/login", new LoginRequest( email, password ));
+			var request = new LoginRequest(email, password);
+			var response = await _httpClient.PostAsJsonAsync("/api/auth/login", request);
 			if (response.IsSuccessStatusCode)
 			{
 				authResult = await response.Content.ReadFromJsonAsync<AuthResult>();
@@ -27,7 +28,7 @@ namespace HobbyMatch.App.Services
 		public async Task<HttpResponseMessage> RegisterUserAsync(string username, string email, string password)
 		{
 			var request = new UserRegisterRequest(email, password, username);
-			var response = await _httpClient.PostAsJsonAsync("/api/auth/register", new { username, email, password });
+			var response = await _httpClient.PostAsJsonAsync("/api/auth/register", request);
 			return response;
 		}
 		public async Task<HttpResponseMessage> RegisterBusinessClientAsync(string username, string email, string password, string taxId)
