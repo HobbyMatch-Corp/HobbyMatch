1a35ae0ea51305962e19520b125fe435b5bd099b
DrKuba2003 <kubadrazan2@gmail.com>
2025-04-13 12:27:43 +0200

    Change ensurecreated to migrate

--- HobbyMatchAPI/Program.cs
+++ HobbyMatchAPI/Program.cs
@@ -75,7 +75,7 @@ var app = builder.Build();
 await using (var serviceScope = app.Services.CreateAsyncScope())
 await using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>())
 {
-    await dbContext.Database.EnsureCreatedAsync();
+    await dbContext.Database.MigrateAsync();
 }
 
 if (app.Environment.IsDevelopment())
