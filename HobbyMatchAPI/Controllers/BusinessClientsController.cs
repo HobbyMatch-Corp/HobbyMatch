2c10b14df96ebacf7e252fa89e6e462857ec5950
DrKuba2003 <kubadrazan2@gmail.com>
2025-04-23 21:15:46 +0200

    fixed user controllers

--- HobbyMatchAPI/Controllers/BusinessClientsController.cs
+++ HobbyMatchAPI/Controllers/BusinessClientsController.cs
@@ -2,6 +2,7 @@
 using HobbyMatch.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace HobbyMatch.API.Controllers
 {
@@ -28,7 +29,7 @@ namespace HobbyMatch.API.Controllers
         [HttpGet("{userId}")]
         public async Task<IActionResult> GetUserAsync(int businessClientId)
         {
-            var emailJwt = User.FindFirst("email")?.Value;
+            var emailJwt = User.FindFirst(ClaimTypes.Email)?.Value;
 
             var businessClient = await _businessClientService.GetBusinessClientByIdAsync(businessClientId);
             if (businessClient == null || string.IsNullOrEmpty(emailJwt) || businessClient.Email != emailJwt) return BadRequest();
@@ -40,7 +41,7 @@ namespace HobbyMatch.API.Controllers
         [HttpPost("{businessClientId}")]
         public async Task<IActionResult> UpdateUserAsync(int businessClientId, [FromBody] BusinessClient businessClient)
         {
-            var emailJwt = User.FindFirst("email")?.Value;
+            var emailJwt = User.FindFirst(ClaimTypes.Email)?.Value;
 
             var businessClientDb = await _businessClientService.GetBusinessClientByIdAsync(businessClientId);
             if (businessClientDb == null || string.IsNullOrEmpty(emailJwt) || businessClientDb.Email != emailJwt) return BadRequest();
