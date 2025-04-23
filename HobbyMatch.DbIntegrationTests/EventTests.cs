6c95d63cd59d5215eb37b0f11d28c72b771c88cd
DrKuba2003 <kubadrazan2@gmail.com>
2025-04-23 13:22:36 +0200

    fixed tests

--- HobbyMatch.DbIntegrationTests/EventTests.cs
+++ HobbyMatch.DbIntegrationTests/EventTests.cs
@@ -16,11 +16,11 @@ namespace HobbyMatch.DbIntegrationTests
         public async Task GetSigndUpEvents_ValidSeededData_ShouldPass()
         {
             // Arrange
-            var user = new User() { Email = "user1@test.com" };
+            string userEmail = "user1@test.com";
             var eventRepo = new EventRepository(DbContext);
 
             // Act
-            var res = await eventRepo.GetSignedUpEvents(user);
+            var res = await eventRepo.GetSignedUpEventsAsync(userEmail);
 
             // Assert
             Assert.NotNull(res);
@@ -31,11 +31,11 @@ namespace HobbyMatch.DbIntegrationTests
         public async Task GetOrganizedEvents_ValidSeededData_ShouldPass()
         {
             // Arrange
-            var organizer = new User() { Email = "user1@test.com" };
+            string organizerEmail = "user1@test.com";
             var eventRepo = new EventRepository(DbContext);
 
             // Act
-            var res = await eventRepo.GetOrganizedEvents(organizer);
+            var res = await eventRepo.GetOrganizedEventsAsync(organizerEmail);
 
             // Assert
             Assert.NotNull(res);
@@ -46,11 +46,11 @@ namespace HobbyMatch.DbIntegrationTests
         public async Task GetSponsoredEvents_ValidSeededData_ShouldPass()
         {
             // Arrange
-            var bc = new BusinessClient() { Email = "bclient1@test.com" };
+            var bcEmail = "bclient1@test.com";
             var eventRepo = new EventRepository(DbContext);
 
             // Act
-            var res = await eventRepo.GetSponsoredEvents(bc);
+            var res = await eventRepo.GetSponsoredEventsAsync(bcEmail);
 
             // Assert
             Assert.NotNull(res);
