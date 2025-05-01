530bee0b1a85715d0c791e27358e98fa4d8d4603
Avesay <juliak@jkkomputer.com.pl>
2025-05-01 10:43:17 +0200

    Add VenueDto

---
+++ HobbyMatch.BL/DTOs/Venues/VenueDto.cs
@@ -0,0 +1,13 @@
+﻿using HobbyMatch.Domain.Entities;
+
+namespace HobbyMatch.BL.DTOs.Venues
+{
+    public record VenueDto(
+    int Id,
+    string Name,
+    string Address,
+    Location Location,
+    decimal Price,
+    int MaxUsers
+    );
+}
