304f4c396072a3ed42967e6ab7cd9c82eff5ba61
Aleksander Bielicki <aleksanderbielicki2003@gmail.com>
2025-05-04 11:26:41 +0200

    Front end logic

--- HobbyMatchApp/Pagination/PaginatedQueryHandler.cs
+++ HobbyMatchApp/Pagination/PaginatedQueryHandler.cs
@@ -1,44 +1,45 @@
 using HobbyMatch.Database.Common.Pagination;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace HobbyMatch.App.Pagination;
 
 public abstract class PaginatedQueryHandler<T>
 {
-    public List<T> Data { get; set; } = [];
-    
-    public int CurrentPage { get; set; }
-    public int PageSize { get; set; }
-    public int TotalPages { get; set; }
-    public bool IsLoading { get; set; }
-    private readonly Func<PaginationParameters, Task<PaginatedData<T>>> _fetchFunc;
     private readonly Action _onChanged;
-    
-    protected PaginatedQueryHandler(Func<PaginationParameters, Task<PaginatedData<T>>> fetchFunc,Action onChanged, int pageSize)
+
+    protected PaginatedQueryHandler(Action onChanged,
+        int pageSize)
     {
-        _fetchFunc = fetchFunc;
         _onChanged = onChanged;
         PageSize = pageSize;
         CurrentPage = 1;
     }
 
+    public List<T> Data { get; set; } = [];
+
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public bool IsLoading { get; set; }
+
+    protected abstract Task<PaginatedData<T>> FetchPageAsync(PaginationParameters parameters);
+
     public async Task LoadPageAsync(int page)
     {
         if (IsLoading)
             return;
-        
+
         IsLoading = true;
-        
+
         _onChanged.Invoke();
-        
-        var result = await _fetchFunc(new PaginationParameters(PageNumber: page,PageSize: PageSize));
-        
+
+        var result = await FetchPageAsync(new PaginationParameters(page, PageSize));
+
         ModifyCurrentData(result);
         CurrentPage = result.CurrentPage;
         TotalPages = result.TotalPages;
-        
+
         IsLoading = false;
-        
+
         _onChanged.Invoke();
     }
 
@@ -54,5 +55,9 @@ public abstract class PaginatedQueryHandler<T>
             await LoadPageAsync(CurrentPage - 1);
     }
 
-    protected abstract void ModifyCurrentData(PaginatedData<T> newData);
+    // This can be modified if another way of modifying data (e.g appending) is added
+    protected void ModifyCurrentData(PaginatedData<T> newData)
+    {
+        Data = newData.Data;
+    }
 }
---
+++ HobbyMatchApp/Services/Venues/IVenueApiService.cs
@@ -0,0 +1,16 @@
+using HobbyMatch.BL.DTOs.Venues;
+using HobbyMatch.Database.Common.Pagination;
+using HobbyMatch.Domain.Requests;
+
+namespace HobbyMatch.App.Services.Venues;
+
+public interface IVenueApiService
+{
+    public Task<PaginatedData<VenueDto>> GetClientVenuesAsync(PaginationParameters paginationParameters);
+    public Task<VenueDetailsDto?> GetVenueByIdAsync(int venueId);
+
+    public Task<PaginatedData<VenueDto>> GetFilteredVenues(string? filter,
+        PaginationParameters paginationParameters);
+
+    public Task<VenueDetailsDto?> CreateVenueAsync(CreateVenueRequest request);
+}
