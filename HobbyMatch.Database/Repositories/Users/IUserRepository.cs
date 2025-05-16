e8dd0ba5322d536dd8d3046de955a712553d3633
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2025-05-16 03:55:11 +0200

    Change repository structure

--- HobbyMatch.Database/Repositories/Users/IUserRepository.cs
+++ HobbyMatch.Database/Repositories/Users/IUserRepository.cs
@@ -5,4 +5,7 @@ namespace HobbyMatch.Database.Repositories.Users;
 public interface IUserRepository
 {
     public Task<Organizer?> GetUserByRefreshTokenAsync(string refreshToken);
+    public Task<Organizer?> GetUserByIdAsync(int id);
+	Task<bool> AddFriendToUserAsync(User user, User friend);
+	Task<bool> RemoveFriendFromUserAsync(User user, User friend);
 }
--- HobbyMatch.Database/Repositories/Users/UserRepository.cs
+++ HobbyMatch.Database/Repositories/Users/UserRepository.cs
@@ -19,4 +19,25 @@ public class UserRepository : IUserRepository
 
         return user;
     }
+	public async Task<Organizer?> GetUserByIdAsync(int id)
+	{
+		var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+		return user;
+	}
+	public async Task<bool> AddFriendToUserAsync(User user, User friend)
+	{
+		if (user.Friends.Any(u => u.Id == friend.Id)) return false;
+		user.Friends.Add(friend);
+		await _context.SaveChangesAsync();
+		return true;
+	}
+
+	public async Task<bool> RemoveFriendFromUserAsync(User user, User friend)
+	{
+		var existing = user.Friends.FirstOrDefault(u => u.Id == friend.Id);
+		if (existing == null) return false;
+		user.Friends.Remove(existing);
+		await _context.SaveChangesAsync();
+		return true;
+	}
 }
