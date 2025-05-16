d87aaa412025e69cac64af114b8bca7db6624034
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2025-05-16 03:57:08 +0200

    Revert "Change repository structure"
    This reverts commit e8dd0ba5322d536dd8d3046de955a712553d3633.

--- HobbyMatch.Database/Repositories/Users/IUserRepository.cs
+++ HobbyMatch.Database/Repositories/Users/IUserRepository.cs
@@ -5,7 +5,4 @@ namespace HobbyMatch.Database.Repositories.Users;
 public interface IUserRepository
 {
     public Task<Organizer?> GetUserByRefreshTokenAsync(string refreshToken);
-    public Task<Organizer?> GetUserByIdAsync(int id);
-	Task<bool> AddFriendToUserAsync(User user, User friend);
-	Task<bool> RemoveFriendFromUserAsync(User user, User friend);
 }
