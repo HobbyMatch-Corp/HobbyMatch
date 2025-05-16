fe95ec51cd859d9b3c9dc0429bcc6527b0e3d108
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2025-05-16 03:14:52 +0200

    Add Api Service

---
+++ HobbyMatchApp/Services/Friends/FriendApiService.cs
@@ -0,0 +1,32 @@
+﻿namespace HobbyMatch.App.Services.Friends
+{
+	public class FriendApiService : IFriendApiService
+	{
+		private readonly HttpClient _httpClient;
+		public FriendApiService(IHttpClientFactory httpClientFactory)
+		{
+			_httpClient = httpClientFactory.CreateClient("AuthenticatedClient");
+		}
+		public async Task<bool> AddFriendAsync(int friendId)
+		{
+			bool success = false;
+			var response = await _httpClient.PostAsJsonAsync("friends/add", friendId);
+			if (response.IsSuccessStatusCode)
+			{
+				success = await response.Content.ReadFromJsonAsync<bool>();
+			}
+			return success;
+		}
+
+		public async Task<bool> RemoveFriendAsync(int friendId)
+		{
+			bool success = false;
+			var response = await _httpClient.PostAsJsonAsync("friends/remove", friendId);
+			if (response.IsSuccessStatusCode)
+			{
+				success = await response.Content.ReadFromJsonAsync<bool>();
+			}
+			return success;
+		}
+	}
+}
---
+++ HobbyMatchApp/Services/Friends/IFriendApiService.cs
@@ -0,0 +1,8 @@
+﻿namespace HobbyMatch.App.Services.Friends
+{
+	public interface IFriendApiService
+	{
+		Task<bool> AddFriendAsync(int friendId);
+		Task<bool> RemoveFriendAsync(int friendId);
+	}
+}
