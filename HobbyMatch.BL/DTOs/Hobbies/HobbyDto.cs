89bc4bf2f68e0d3742cf0cf715cc64d2741bb84c
DrKuba2003 <kubadrazan2@gmail.com>
2025-05-18 16:07:05 +0200

    added hobby controller

---
+++ HobbyMatch.BL/DTOs/Hobbies/HobbyDto.cs
@@ -0,0 +1,16 @@
+﻿using HobbyMatch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyMatch.BL.DTOs.Hobbies
+{
+    public record HobbyDto(string name);
+
+    public static partial class HobbyExtensions
+    {
+        public static HobbyDto ToDto(this Hobby hobby) => new HobbyDto(hobby.Name);
+    }
+}
