4077a15c5c593462611420d7dcd3fd00590445ad
DrKuba2003 <kubadrazan2@gmail.com>
2025-05-18 15:50:42 +0200

    started hobby service

---
+++ HobbyMatch.BL/Services/Hobbies/HobbyService.cs
@@ -0,0 +1,18 @@
+﻿using HobbyMatch.Domain.Entities;
+
+namespace HobbyMatch.BL.Services.Hobbies
+{
+    public class HobbyService : IHobbyService
+    {
+
+        public Task<ICollection<Hobby>> GetHobbiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hobby?> GetHobbyAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HobbyMatch.BL/Services/Hobbies/IHobbyService.cs
@@ -0,0 +1,10 @@
+﻿using HobbyMatch.Domain.Entities;
+
+namespace HobbyMatch.BL.Services.Hobbies
+{
+    public interface IHobbyService
+    {
+        public Task<ICollection<Hobby>> GetHobbiesAsync();
+        public Task<Hobby?> GetHobbyAsync(int id);
+    }
+}
