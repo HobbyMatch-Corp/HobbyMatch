ad2631981bcddb4d4aa0bd756daf617a00b210d9
DrKuba2003 <kubadrazan2@gmail.com>
2025-05-18 15:22:32 +0200

    added hobby repo

---
+++ HobbyMatch.Database/Repositories/Hobbies/IHobbyRepository.cs
@@ -0,0 +1,10 @@
+﻿using HobbyMatch.Domain.Entities;
+
+namespace HobbyMatch.Database.Repositories.Hobbies
+{
+    internal interface IHobbyRepository
+    {
+        public Task<ICollection<Hobby>> GetHobbiesAsync();
+        public Task<Hobby?> GetHobbyAsync(int id);
+    }
+}
