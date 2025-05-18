6dd73db970a3498aa44c52ab6bee6fd505ec9b7c
DrKuba2003 <kubadrazan2@gmail.com>
2025-05-18 17:00:21 +0200

    added hobby services to frontend

---
+++ HobbyMatchApp/Services/Hobbies/IHobbyApiService.cs
@@ -0,0 +1,9 @@
+﻿using HobbyMatch.BL.DTOs.Hobbies;
+
+namespace HobbyMatch.App.Services.Hobbies
+{
+    public interface IHobbyApiService
+    {
+        public Task<ICollection<HobbyDto>> GetHobbiesAsync();
+    }
+}
