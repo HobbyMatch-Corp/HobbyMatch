f46504cce2bfd59172ced3e5cdb752008c02a60f
DrKuba2003 <kubadrazan2@gmail.com>
2025-05-18 15:16:46 +0200

    add hobby table and ef migrations

--- HobbyMatch.Database/Migrations/20250518131620_AddHobbies.Designer.cs
+++ HobbyMatch.Database/Migrations/20250518131620_AddHobbies.Designer.cs
@@ -5,6 +5,7 @@ using HobbyMatch.Database.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -12,9 +13,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HobbyMatch.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250518131620_AddHobbies")]
+    partial class AddHobbies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
