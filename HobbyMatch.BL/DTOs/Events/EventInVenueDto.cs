8b579b3a0d4dfa1a7aab0ca03d15b3780ca39a37
Avesay <juliak@jkkomputer.com.pl>
2025-05-26 10:16:22 +0200

    Update elements that use CreateVenueDto

--- HobbyMatch.BL/DTOs/Events/EventInVenueDto.cs
+++ HobbyMatch.BL/DTOs/Events/EventInVenueDto.cs
@@ -1,11 +1,6 @@
 ﻿using HobbyMatch.BL.DTOs.Events;
 using HobbyMatch.BL.DTOs.Organizers;
 using HobbyMatch.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HobbyMatch.BL.DTOs.Events
 {
