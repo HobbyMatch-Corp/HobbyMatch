ea7ad1eb1dbe861f02d00277eeec6d72dd414aa3
Avesay <juliak@jkkomputer.com.pl>
2025-05-26 12:15:32 +0200

    quick fix

--- HobbyMatch.BL/DTOs/Venues/VenueDto.cs
+++ HobbyMatch.BL/DTOs/Venues/VenueDto.cs
@@ -17,6 +17,6 @@ public static partial class VenueExtensions
 
     public static VenueDto ToDto(this Venue venue)
     {
-        return new VenueDto($"{venue.Id}", venue.Name, venue.Description, venue.Address, venue.Location, new OrganizerDto($"{venue.BusinessClientId}", venue.BusinessClient.UserName ?? ""));
+        return new VenueDto($"{venue.Id}", venue.Name, venue.Description, venue.Address, venue.Location, new OrganizerDto($"{venue.BusinessClientId}", venue?.BusinessClient?.UserName ?? ""));
     }
 }
